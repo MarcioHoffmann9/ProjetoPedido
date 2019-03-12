06feb73a9a5f14c9c5cddb855e57f0b8d55e55a6
Márcio <marciohoffmann9@gmail.com>
2019-03-11 22:12:46 -0300

    Criação do repositoria para categoria

--- src/main/java/br/senai/sc/ProjetoPedidoApplication.java
+++ src/main/java/br/senai/sc/ProjetoPedidoApplication.java
@@ -1,13 +1,33 @@
 package br.senai.sc;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.senai.sc.domain.Categoria;
+import br.senai.sc.repositories.CategoriaRepository;
+
+
 @SpringBootApplication
-public class ProjetoPedidoApplication {
+public class ProjetoPedidoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPedidoApplication.class, args);
+	}
+	@Autowired
+	public CategoriaRepository repo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria a = new Categoria(null, "Informatica");
+		Categoria b = new Categoria(null, "Informatica");
+		
+		repo.save(a);
+		repo.save(b);
 	}
 
 }
--- src/main/java/br/senai/sc/domain/Categoria.java
+++ src/main/java/br/senai/sc/domain/Categoria.java
@@ -1,7 +1,15 @@
 package br.senai.sc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //Cria a tabela no banco.
 public class Categoria {
 
+	@Id // usado para identificar o Id da tabela, sera o atributo que estiver abaixo da anotação.
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // diz que o Id é o gerador e "AUTO INCREMENT".
 	private Integer id;
 	private String nome;
 	
